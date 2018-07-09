29db689d20a230c04836d51ca0af204060a1ef0d
Stephanie Chau <schau@uc.cl>
2018-07-09 12:45:10 -0400

    added configuration, params and main functions

---
+++ config.py
@@ -0,0 +1,5 @@
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/spreadsheets']
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'QR-Classifier-v1'
