199e6fa57352107ec326587a08546c1ed762767b
Stephanie Chau <schau@uc.cl>
2018-07-10 00:36:08 -0400

    deleted files

--- config.py
+++
@@ -1,5 +0,0 @@
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/spreadsheets']
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'QR-Classifier-v1'
