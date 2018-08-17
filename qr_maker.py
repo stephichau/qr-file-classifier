2be22e2f0acfa13b6c583cfe7e74247cb58cdb89
Stephanie Chau <schau@uc.cl>
2018-08-17 12:16:27 -0300

    fixed previous commit buggs

--- qr_maker.py
+++ qr_maker.py
@@ -12,12 +12,12 @@ Get data from command line
 
 def get_data_from_file(file_path):
     with open(file_path, 'r') as file:
-        file = json.load(file)
-    return file
+        data = json.load(file)
+    return data
 
 def check_data_in_file(data_dict):
     for key in data_dict:
-        if key in ['course', 'section', 'year', 'semester', 'evaluation', 'number'] and data_dict[key] = '':
+        if key in ['course', 'section', 'year', 'semester', 'evaluation', 'number'] and data_dict[key] == '':
             return False
     return True
 
@@ -29,13 +29,13 @@ def parse_arguments():
     parser.add_argument('-sem', "--semester", action="store", dest="semester_number", help="Semester number: 1 or 2")
     parser.add_argument('-e', "--evaluation", action="store", dest="evaluation_name", help="Evaluation name: i1, i2, i3, exam")
     parser.add_argument('-n', "--number", action="store", dest="number_qr",help="Numbers of correlated qr: Ex. 10")
-    parser.add_argument('-n', "--file", action="store", dest="file_data",help="File where data is stored in JSON format")
+    parser.add_argument('-f', "--file", action="store", dest="file_data",help="File where data is stored in JSON format")
     return parser.parse_args()
 
 
 # Version 0
-QR_DIRECTORY_PATH = '{}/QR/'.format(os.path.abspath('.'));
-print(QR_DIRECTORY_PATH)
+QR_DIRECTORY_PATH = '{}/QR'.format(os.path.abspath('.'));
+
 def create_qrs(course, section, year, semester, evaluation_name, number, *args):
     """
     Creates n *.png qr in format: course-section-year-semester-number and saves them in directory QR_DIRECTORY_PATH/evaluation_name/
@@ -67,20 +67,22 @@ def create_qrs(course, section, year, semester, evaluation_name, number, *args):
 
 def main():
     arguments = parse_arguments()
-    if not (arguments.course_name and arguments.section_number and arguments.year_number and
-        arguments.semester_number and arguments.evaluation_name and arguments.number_qr and arguments.file_data):
-        error_message = "Invalid arguments. Missing either --course, --section or --year or --semester or --evaluation_name or --number or --file."
-        cool_print_decoration(error_message, style = 'alert')
-    elif arguments.file_data and not (arguments.course_name and arguments.section_number and arguments.year_number and
+    if arguments.file_data and not (arguments.course_name and arguments.section_number and arguments.year_number and
         arguments.semester_number and arguments.evaluation_name and arguments.number_qr):
         data = get_data_from_file(arguments.file_data)
         if not(check_data_in_file(data)):
             error_message = "Invalid arguments in file: {}. Please check file.".format(arguments.file_data)
             cool_print_decoration(error_message, style='alert')
+        elif not os.path.exists(QR_DIRECTORY_PATH):
+            error_message = "Invalid QR DIRECTORY PATH: {}".format(QR_DIRECTORY_PATH)
+            cool_print_decoration(error_message, style = 'alert')
         else:
             data = [ data['course'], data['section'], data['year'], data['semester'], data['evaluation'], int(data['number']) ]
             create_qrs(*data)
-
+    elif not (arguments.course_name and arguments.section_number and arguments.year_number and
+        arguments.semester_number and arguments.evaluation_name and arguments.number_qr and arguments.file_data):
+        error_message = "Invalid arguments. Missing either --course, --section or --year or --semester or --evaluation_name or --number or --file."
+        cool_print_decoration(error_message, style = 'alert')
     else:
         create_qrs(arguments.course_name, arguments.section_number, arguments.year_number,
         arguments.semester_number, arguments.evaluation_name, int(arguments.number_qr))
