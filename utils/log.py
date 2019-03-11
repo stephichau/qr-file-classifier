276bd1840141abc54a489be6ac5d5341b9801700
Stephanie Chau <schau@uc.cl>
2019-03-10 18:34:53 -0700

    Added new color, print invalid messages and progress bar

--- utils/log.py
+++ utils/log.py
@@ -9,6 +9,7 @@ colors = {'normal': (Fore.RESET,),
           'result': (Fore.GREEN,),
           'info': (Fore.CYAN,),
           'danger': (Fore.RED,),
+          'progress':(Fore.MAGENTA,),
 }
 
 def cool_print_decoration(message, style, separator='-'):
@@ -27,3 +28,17 @@ def cool_print(*args, style='normal', **kwargs):
         with open('alerts.log', 'a') as f:
             f.write(repr(args))
             f.write("\n")
+
+def print_invalid_file(_file: str) -> None:
+    _msg = f'Invalid file {_file}\nPlease check file and try again'
+    cool_print_decoration(_msg, 'danger')
+
+# https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+def print_progress_bar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+
+    percent = ('{0:.' + str(decimals) + 'f}').format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    cool_print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r', style='progress')
+
+    print('\n') if iteration == total else None
