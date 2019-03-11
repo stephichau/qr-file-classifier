ca900c88633cc5b5b9d509897d462f52455ae5cd
Stephanie Chau <schau@uc.cl>
2019-03-10 18:36:22 -0700

    Modified package __init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,7 @@ from .parse_arguments import *
 from .log import *
 from .file_converter import *
 from .file_merger import *
+from.json_reader import *
+
+def create_directory(dir_path: str) -> None:
+    os.mkdir(dir_path)
